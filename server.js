95fa9ae627ef0feb38f7cf0805e81c2dd97bfbcf
nithin <nithinreddy1572@gmail.com>
2023-02-13 23:55:45 +0530

    changes

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5010;
-
 app.get("/", function(req, res){
 	// res.send("I Am Nithin");
 	res.sendFile(__dirname+"/index.html")
@@ -10,7 +9,6 @@ app.get("/resume", function(req, res){
 	// res.send("I Am Nithin");
 	res.sendFile(__dirname+"/resume.html")
 });
-
 app.listen(port, function(){
 	console.log("Server running on http://localhost:"+port);
 	console.log(`Server running on http://localhost:${port}`);
