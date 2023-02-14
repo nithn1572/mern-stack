383436e2715799672957285187fe5133f2f5b81e
nithin <nithinreddy1572@gmail.com>
2023-02-14 19:41:26 +0530

    changes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+var path = require('path');//for css 
 const port = process.env.PORT || 5010;
 app.get("/", function(req, res){
 	// res.send("I Am Nithin");
@@ -9,7 +10,8 @@ app.get("/resume", function(req, res){
 	// res.send("I Am Nithin");
 	res.sendFile(__dirname+"/resume.html")
 });
-app.use(express.static(__dirname+'/style_resume.css'));
+app.use(express.static(path.join(__dirname+'MERN-STACK')));
+
 app.listen(port, function(){
 	console.log("Server running on http://localhost:"+port);
 	console.log(`Server running on http://localhost:${port}`);
