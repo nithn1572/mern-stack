fee81428078f893405f803b0d427b356f061bec1
nithin <nithinreddy1572@gmail.com>
2023-02-14 23:38:09 +0530

    change

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path'); 
 const port = process.env.PORT || 5010;
+app.use(express.static("public", options));
 const options = {
 	extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
 	index: ['index.html'],  
