488f63418eddc766be63b4935c3f736ad7a7121c
nithin <nithinreddy1572@gmail.com>
2023-02-14 23:45:42 +0530

    a

--- server.js
+++ server.js
@@ -2,11 +2,6 @@ const express = require('express');
 const app = express();
 const path = require('path'); 
 const port = process.env.PORT || 5010;
-app.use(express.static("public", options));
-const options = {
-	extensions: ['htm', 'html','css','js','ico','jpg','jpeg','png','svg'],
-	index: ['index.html'],  
-}
 app.get("/", function(req, res){
 	res.sendFile(__dirname+"/index.html")
 });
@@ -16,6 +11,7 @@ app.get("/resume", function(req, res){
 app.get("/card",function(req,res){
 	res.sendFile(__dirname+"/card.html")
 })
+app.use(express.static("public"));
 app.listen(port, function(){
 	console.log("Server running on http://localhost:"+port);
 	console.log(`Server running on http://localhost:${port}`);
