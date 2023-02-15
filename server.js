0e54c33a5d43eef03ae85d0a215f5d4ac9861abc
nithin <nithinreddy1572@gmail.com>
2023-02-15 23:55:49 +0530

    changed

--- server.js
+++ server.js
@@ -1,29 +1,42 @@
 require('dotenv').config();
-const mongoose = require("mongoose");	
+const userLib = require("./backend/lib/userLib");
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-const path = require('path');
 const port = process.env.PORT || 5010;
-app.get("/", function(req, res){
-	res.sendFile(__dirname+"/index.html")
+app.use(express.static(__dirname + '/public'));
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
 });
-app.get("/resume", function(req, res){
-	res.sendFile(__dirname+"/resume.html")
+app.get('/resume', function(req, res) {
+    res.sendFile(__dirname + '/resume.html');
 });
-app.get("/card",function(req,res){
-	res.sendFile(__dirname+"/card.html")
-})
-app.use(express.static("public"));
+app.get('/card', function(req, res) {
+    res.sendFile(__dirname + '/card.html');
+});
 mongoose.set('strictQuery', true);
-mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function(err){
-	if(err){
-		console.error(err);
-	}
-	else{
-		console.log("DB CONNECTED");
-		app.listen(port, function(){
-			console.log("Server running on http://localhost:"+port);
-			console.log(`Server running on http://localhost:${port}`);
-		});		
-	}
+mongoose.connect(process.env.MONGO_CONNECTION_STRING, {}, function(err) {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log('Connected to database');
+        userLib.getAllUsers(function(err, usersList) {
+            if (err) {
+                console.error(err);
+            } else {
+                if (usersList.length === 0) {
+                    userLib.createFirstUser(function(err, res) {
+                        if (err) {
+                            console.error(err);
+                        } else {
+                            console.log(res);
+                        }
+                    });
+                }
+            }
+        });
+        app.listen(port, function() {
+            console.log('Server started on port ' + port);
+        });
+    }
 });
