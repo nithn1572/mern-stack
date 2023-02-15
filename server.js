7e29c369816485e1d35b52f35f17436dc442699b
nithin <nithinreddy1572@gmail.com>
2023-02-15 22:00:46 +0530

    changes

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+const mongoose = require("mongoose");	
 const express = require('express');
 const app = express();
-const path = require('path'); 
+const path = require('path');
 const port = process.env.PORT || 5010;
 app.get("/", function(req, res){
 	res.sendFile(__dirname+"/index.html")
@@ -12,7 +14,16 @@ app.get("/card",function(req,res){
 	res.sendFile(__dirname+"/card.html")
 })
 app.use(express.static("public"));
-app.listen(port, function(){
-	console.log("Server running on http://localhost:"+port);
-	console.log(`Server running on http://localhost:${port}`);
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function(err){
+	if(err){
+		console.error(err);
+	}
+	else{
+		console.log("DB CONNECTED");
+		app.listen(port, function(){
+			console.log("Server running on http://localhost:"+port);
+			console.log(`Server running on http://localhost:${port}`);
+		});		
+	}
 });
