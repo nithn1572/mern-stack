9254cca6f84f4068ea5d0917c10483ab71a0f61c
nithin <nithinreddy1572@gmail.com>
2023-02-16 22:09:01 +0530

    updated

--- server.js
+++ server.js
@@ -1,53 +1,87 @@
-require('dotenv').config();
+require("dotenv").config();
 const userLib = require("./backend/lib/userLib");
+const mongoose = require("mongoose");
 const express = require('express');
-const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 5010;
-app.use(express.static(__dirname + '/public'));
-app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/index.html');
+const options = {
+	extensions:['htm','html','css','js','ico','jpg','jpeg','png','svg','pdf'],
+	index:['index.html'],
+}
+app.use(express.static("public",options));
+app.get("/", function(req, res){
+	res.sendFile(__dirname+"/index.html");
 });
-app.get('/resume', function(req, res) {
-    res.sendFile(__dirname + '/resume.html');
+
+app.get("/resume", function(req, res){
+	res.sendFile(__dirname+"/resume.html");
 });
-app.get('/card', function(req, res) {
-    res.sendFile(__dirname + '/card.html');
-});
-app.get('/neetCode', function(req, res) {
-    res.sendFile(__dirname + '/NeetCode.html');
+
+app.get("/card", function(req, res){
+	res.sendFile(__dirname+"/card.html");
 });
 mongoose.set('strictQuery', true);
-mongoose.connect(process.env.MONGO_CONNECTION_STRING, {}, function(err) {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log('Connected to database');
-        // userLib.getAllUsers(function(err, usersList) {
-        //     if (err) {
-        //         console.error(err);
-        //     } else {
-        //         if (usersList.length === 0) {
-        //             userLib.createFirstUser(function(err, res) {
-        //                 if (err) {
-        //                     console.error(err);
-        //                 } else {
-        //                     console.log(res);
-        //                 }
-        //             });
-        //         }
-        //     }
-        // });
-        userLib.updateUser(function(err,result){
-            if(err){
-                console.error(err);
-            }
-            else{
-                console.log(result);
-            }
-        });
-        app.listen(port, function() {
-            console.log('Server started on port ' + port);
-        });
-    }
+mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function (err){
+	if(err){
+		console.error(err);
+	}
+	else{
+		console.log("DB Connected");
+		// TODO: donot create user if atleast 1 user exist int the table
+		// userLib.createFirstUser(function(err,res){
+		// 	if(err){
+		// 		// console.error(err);
+		// 	}
+		// 	else{
+		// 		console.log(res);
+		// 	}
+		// });
+		// userLib.createUser({userName: "nithin1572", yearOfGraduation: 2024},function(err,result){
+		// 	if(err){
+		// 		console.error(err);
+		// 	}
+		// 	else{
+		// 		console.log(result);
+		// 	}
+		// });
+		// userLib.updateUser(function(err,result){
+		// 	if(err){
+		// 		console.error(err);
+		// 	}
+		// 	else{
+		// 		console.log(result);
+		// 	}
+		// });
+		// userLib.deleteUser("nithin1572",function(err,result){
+		// 	if(err){
+		// 		console.error(err);
+		// 	}
+		// 	else{
+		// 		console.log(result);
+		// 	}
+		// });
+		// userLib.getUserByFilter({userName: "nithin1572"}, function(err,result){
+		// 	if(err){
+		// 		console.error(err);
+		// 	}
+		// 	else{
+		// 		console.log(result);
+		// 	}
+		// });
+		// userLib.getAllUsers(function(err,result){
+		// 	if(err){
+		// 		console.error(err);
+		// 	}
+		// 	else{
+		// 		console.log(result);
+		// 	}
+		// });
+        
+
+		app.listen(port, function(){
+			console.log("Server running on http://localhost:"+port);
+			console.log(`Server running on http://localhost:${port}`);
+		});
+		
+	}
 });
