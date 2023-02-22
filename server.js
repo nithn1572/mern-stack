0054184720812960e629b7f998a4a65c1fb5ebd2
nithin <nithinreddy1572@gmail.com>
2023-02-22 22:40:25 +0530

    a

--- server.js
+++ server.js
@@ -84,9 +84,6 @@ app.get("/weather", function(req, res){
 app.get("/todo", function(req, res){
 	res.sendFile(__dirname+"/frontend/html/todo.html");
 });
-app.get("/about", function(req, res){
-	res.sendFile(__dirname+"/frontend/html/about.html");
-});
 mongoose.set('strictQuery', true);
 mongoose.connect(process.env.MONGO_CONNECTION_STRING,{},function (err){
 	if(err){
